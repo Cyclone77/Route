78b2c88c10884c190fbee45474f6504f226b16b7
Cyclone77 <fillchar@live.com>
2016-10-19 11:36:54 +0800

    路由例子

---
+++ Js/main.js
@@ -0,0 +1,56 @@
+﻿require.config({
+    paths: {
+        jquery: "../lib/jquery",
+        bootstrap: "../lib/bootstrap",
+        Route: "../lib/Route"
+    },
+    shim: {
+        "bootstrap": {
+            deps: ['jquery']
+        }
+    }
+});
+
+require(["jquery", "bootstrap", "Route"], function ($, _, Route) {
+    Route({
+        same: true,
+        animationClass: "fadeInUp",
+        applyDom: $("#divSection")[0],
+        routeArray: [{
+            eventdom: $(".nav.navbar-nav [mark='index']")[0],
+            ajaxget: true,
+            ajaxApplyUrl: "tpl/index.html",
+            urlHash: "#!index",
+            pageTitle: "主页",
+            defaultRoute: true,
+            applycallback: function (that) {                
+                //require(["page1"], function (page1) {
+                //    //alert(sdfx);
+                //});
+            }
+        }, {
+            eventdom: $(".nav.navbar-nav [mark='tag']")[0],
+            ajaxget: true,
+            ajaxApplyUrl: "tpl/tag.html",
+            urlHash: "#!tag",
+            applycallback: function (that) {
+
+            }
+        }, {
+            eventdom: $(".nav.navbar-nav [mark='about']")[0],
+            ajaxget: true,
+            ajaxApplyUrl: "tpl/about.html",
+            urlHash: "#!about",
+            applycallback: function (that) {
+
+            }
+        }],
+        onregistered: function (that) {
+            $(that.eventdom).closest("li").addClass("active");
+        }
+    });
+
+    $(".nav.navbar-nav li").on("click", function () {
+        $(this).addClass("active").siblings("li").removeClass("active");
+    });
+});
